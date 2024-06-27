df9d2aea04bdcc40279e0fd1241e8188e399c6f8
Dmitriyq09 <36569062+Dmitriyq09@users.noreply.github.com>
2024-06-27 22:53:00 +0300

    1

--- src/main/resources/sql/Init.sql
+++ src/main/resources/sql/Init.sql
@@ -3,6 +3,6 @@ values (1, 'ROLE_ADMIN'),
        (2, 'ROLE_USER');
 
 insert into users (id, username, surname, age, password)
-values (1, 'admin', 'admin', 35, '100'),
-       (2, 'user', 'user', 33, '100');
+values (1, 'admin', 'admin', 35, 'admin'),
+       (2, 'user', 'user', 33, 'admin');
 
