4fbd8a4f549c0b403d81e01d55f146ccbeed8206
Dmitriyq09 <36569062+Dmitriyq09@users.noreply.github.com>
2024-06-30 02:18:18 +0300

    2

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.entities.User;
-import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
 import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
@@ -29,55 +28,58 @@ public class AdminController {
     @GetMapping("/registration")
     public String registrationPage(@ModelAttribute("user") User user, Model model) {
         model.addAttribute("user", new User());
-        model.addAttribute("allRoles", roleService.getAllRoles());
-        return "/ADMIN/registration"; // Шаблон Thymeleaf
+        model.addAttribute("roles", roleService.getAllRoles());
+        return "redirect:/users_table"; // Шаблон Thymeleaf
     }
 
     @PostMapping("/registration")
     public String performRegistration(@ModelAttribute("user") @Valid User user,
                                       BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return "/ADMIN/registration"; // Шаблон Thymeleaf
+            return "/ADMIN/users_table"; // Шаблон Thymeleaf
         }
         userService.saveUser(user);
-        return "redirect:/admin";
+        return "redirect:/users_table";
     }
 
     @GetMapping
     public String allUsers(Model model, Principal principal) {
+        User userFrom = userService.getUserByUsername(principal.getName());
+        model.addAttribute("userFrom", userFrom);
         model.addAttribute("users", userService.getAllUsers());
         model.addAttribute("roles", roleService.getAllRoles());
-        User user = userService.getUserByUsername(principal.getName());
-
         return "/ADMIN/users_table"; // Шаблон Thymeleaf
     }
 
+
     @GetMapping("/{id}")
     public String show(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("user", userService.getUserById(id));
-        return "redirect:/admin"; // Шаблон Thymeleaf
+        User user = userService.getUserById(id);
+        model.addAttribute("user", user);
+        model.addAttribute("roles", user.getRoles());
+        return "/ADMIN/users_table"; // Шаблон Thymeleaf
     }
 
     @GetMapping("/{id}/edit")
     public String edit(@PathVariable("id") Long id, Model model) {
         model.addAttribute("user", userService.getUserById(id));
-        model.addAttribute("allRoles", roleService.getAllRoles());
-        return "redirect:/admin"; // Шаблон Thymeleaf
+        model.addAttribute("roles", roleService.getAllRoles());
+        return "redirect:/users_table"; // Шаблон Thymeleaf
     }
 
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("user") @Valid User user, @PathVariable("id") Long id,
                          BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return "/ADMIN/edit"; // Шаблон Thymeleaf
+            return "/ADMIN/users_table"; // Шаблон Thymeleaf
         }
         userService.updateUser(user, id);
-        return "redirect:/admin";
+        return "redirect:/users_table";
     }
 
     @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") Long id) {
         userService.deleteUserById(id);
-        return "redirect:/admin";
+        return "redirect:/users_table";
     }
 }
